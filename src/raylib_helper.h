81ea780e0067d9f06e436c48d3933be572eb3648
Plucky Porcupine <pluckyporcupinegb@gmail.com>
2020-03-20 10:48:00 -0400

    explicitly typed enum members, added eq for enums and some structs, changed module names

--- src/raylib_helper.h
+++ src/raylib_helper.h
@@ -6,24 +6,48 @@ Vector2 Vector2_copy(Vector2 *v) {
     return *v;
 }
 
+bool Vector2__EQ_(Vector2 v1, Vector2 v2) {
+    return ((v1.x == v2.x) && (v1.y == v2.y));
+}
+
 Vector3 Vector3_copy(Vector3 *v) {
     return *v;
+}
+
+bool Vector3__EQ_(Vector3 v1, Vector3 v2) {
+    return ((v1.x == v2.x) && (v1.y == v2.y) && (v1.z == v2.z));
 }
 
 Vector4 Vector4_copy(Vector4 *v) {
     return *v;
 }
 
+bool Vector4__EQ_(Vector4 v1, Vector4 v2) {
+    return ((v1.x == v2.x) && (v1.y == v2.y) && (v1.z == v2.z) && (v1.w == v2.w));
+}
+
 Quaternion Quaternion_copy(Quaternion *q) {
     return *q;
+}
+
+bool Quaternion__EQ_(Quaternion q1, Quaternion q2) {
+    return ((q1.x == q2.x) && (q1.y == q2.y) && (q1.z == q2.z) && (q1.w == q2.w));
 }
 
 Color Color_copy(Color *c) {
     return *c;
 }
 
+bool Color__EQ_(Color c1, Color c2) {
+    return ((c1.r == c2.r) && (c1.g == c2.g) && (c1.b == c2.b) && (c1.a == c2.a));
+}
+
 Rectangle Rectangle_copy(Rectangle *r) {
     return *r;
+}
+
+bool Rectangle__EQ_(Rectangle r1, Rectangle r2) {
+    return ((r1.x == r2.x) && (r1.y == r2.y) && (r1.width == r2.width) && (r1.height == r2.height));
 }
 
 Image Image_copy(Image *i) {
